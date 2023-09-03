2fbe0eb50f00e0147e1720ca15b450365b081f2d
vlsk3104 <vlsk3104@gmail.com>
2023-09-03 15:09:02 +0900

    Navigation sidebar

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { Plus } from 'lucide-react'
+import React from 'react'
+
+import ActionTooltip from '../action-tooltip'
+
+const NavigationAction = () => {
+  return (
+    <div>
+      <ActionTooltip side="right" align="center" label="チャットルーム 追加">
+        <button className="group flex items-center">
+          <div className="mx-3 flex h-[48px] w-[48px] items-center justify-center overflow-hidden rounded-[24px] bg-background transition-all group-hover:rounded-[16px] group-hover:bg-emerald-500 dark:bg-neutral-700">
+            <Plus
+              className="text-emerald-500 transition group-hover:text-white"
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  )
+}
+
+export default NavigationAction
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,62 @@
+import { UserButton } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+
+import { ModeToggle } from '../mode-toggle'
+import { ScrollArea } from '../ui/scroll-area'
+import { Separator } from '../ui/separator'
+
+import NavigationAction from './navigation-action'
+import NavigationItem from './navigation-item'
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    redirect('/')
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  })
+
+  return (
+    <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-center text-primary dark:bg-[#1E1F22]">
+      <NavigationAction />
+      <Separator className="mx-auto h-2 w-10 rounded-md bg-zinc-300 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="mt-auto flex flex-col items-center gap-y-4 pb-3">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: 'h-[40px] w-[40px]',
+            },
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NavigationSidebar
