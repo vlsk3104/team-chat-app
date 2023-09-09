7be9e34361d627263bbc5825ef7b0d1c2dad46f4
vlsk3104 <vlsk3104@gmail.com>
2023-09-09 12:09:13 +0900

    Server Sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+import ServerSidebar from '@/components/server/server-sidebar'
+
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db'
+
+const SeverIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: { serverId: string }
+}) => {
+  const profile = await currentProfile()
+
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  })
+
+  if (!server) {
+    return redirect('/')
+  }
+
+  return (
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-20 hidden h-full w-60 flex-col md:flex">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  )
+}
+
+export default SeverIdLayout
