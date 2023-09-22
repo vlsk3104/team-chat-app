84bc1830a72b5284c96d09f5287b86de870aa333
vlsk3104 <vlsk3104@gmail.com>
2023-09-22 17:28:57 +0900

    @clerk/localizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ModalProvider from '@/components/providers/modal-provider'
 import QueryProvider from '@/components/providers/query-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { jaJP } from '@clerk/localizations'
 
 import { cn } from '@/lib/utils'
 
@@ -28,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={jaJP}>
       <html lang="ja" suppressHydrationWarning>
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
